e6bab0a8fad6374f18e7560292addf374e1f0fe6
Blazej Bucko <blazej.bucko@gmail.com>
2017-12-22 21:53:19 +0100

    Added support for CONNECT packet

--- src/main.rs
+++ src/main.rs
@@ -2,53 +2,102 @@ use std::net;
 use std::io::{Read, Write};
 use std::thread;
 use std::sync::{Mutex, Arc};
+use std::result;
 
-#[derive(Debug)]
-struct PacketHeader {}
-
-impl PacketHeader {
-    fn new() -> PacketHeader {
-        PacketHeader {}
-    }
-}
-
-#[derive(Debug)]
-struct Packet {
-    header: PacketHeader,
-}
-
-impl Packet {
-    fn new() -> Packet {
-        Packet { header: PacketHeader::new() }
-    }
-}
+type Result<T> = result::Result<T, &'static str>;
 
 fn handle_client(stream: &mut net::TcpStream) {
     println!("new client: {:?}", stream);
 
     loop {
-        let mut packet_type = [0];
-        let _ = stream.read_exact(&mut packet_type);
+        let mut fixed_header = [0];
+        let _ = stream.read_exact(&mut fixed_header);
 
-        let mut flags = [0, 0];
-        let _ = stream.read_exact(&mut flags);
-        println!("data: {:?}, flags: {:?}", packet_type, flags);
+        let packet_type = fixed_header[0] >> 4;
+        let flags = fixed_header[0] & 0b00001111;
 
-        let packet = Packet::new();
-        println!("packet: {:?}", packet);
+        println!(
+            "header: {:08b} packet_type: {:08b} flags: {:08b}",
+            fixed_header[0],
+            packet_type,
+            flags
+        );
 
-        let response = [97, 97, 97, 13, 10];
-        match stream.write(&response) {
-            Err(_) => break,
-            Ok(_) => continue,
+        let remaining_length = variable_length(stream);
+        println!("remaining_length {:?}", remaining_length);
+
+        let mut payload: Vec<u8> = vec![0; remaining_length as usize];
+        let _ = stream.read_exact(&mut payload);
+
+        let response = match packet_type {
+            1 => handle_connect(&mut payload),
+            _ => handle_unknown(&mut payload),
+        };
+
+        match response {
+            Ok(result) => {
+                match stream.write(&result) {
+                    Err(_) => break,
+                    Ok(_) => continue,
+                }
+            }
+            Err(err_msg) => panic!("something went wrong: {:?}", err_msg),
         }
     }
+}
+
+fn handle_connect(payload: &mut Vec<u8>) -> Result<Vec<u8>> {
+    println!("connect payload {:?}", payload);
+    let length = take_length(payload);
+    let protocol_name = take_proto_name(payload, length);
+
+    if protocol_name != "MQTT" {
+        return Err("Invalid protocol name");
+    }
+
+    println!("protocolName: {:?} :: length: {:?}", protocol_name, length);
+
+    Ok(vec![])
+}
+
+fn handle_unknown(payload: &mut Vec<u8>) -> Result<Vec<u8>> {
+    panic!("Unknown payload: {:?}", payload)
+}
+
+fn take_proto_name(payload: &mut Vec<u8>, length: usize) -> std::string::String {
+    let proto_arr: Vec<u8> = payload.drain(0..length).collect();
+    String::from_utf8(proto_arr).expect("Error unwraping proto_name")
+}
+
+fn take_length(payload: &mut Vec<u8>) -> usize {
+    let length_arr: Vec<u8> = payload.drain(0..2).collect();
+    ((length_arr[0] as usize) << 8) | (length_arr[1] as usize)
+}
+
+fn variable_length(stream: &mut net::TcpStream) -> u8 {
+    let mut multiplier = 1;
+    let mut value: u8 = 0;
+    let mut encoded_byte = [0];
+
+    let _ = stream.read_exact(&mut encoded_byte);
+    value += (encoded_byte[0] & 127) * multiplier;
+    multiplier *= 128;
+
+    while encoded_byte[0] & 128 != 0 {
+        let _ = stream.read_exact(&mut encoded_byte);
+        value += (encoded_byte[0] & 127) * multiplier;
+        multiplier *= 128;
+        if multiplier > 128 * 128 * 128 {
+            panic!();
+        }
+    }
+    value
 }
 
 fn main() {
     let data = Arc::new(Mutex::new(vec![1u32, 2, 3]));
 
-    if let Ok(listener) = net::TcpListener::bind("127.0.0.1:8080") {
+    if let Ok(listener) = net::TcpListener::bind("127.0.0.1:1883") {
         println!("Server is listening: {:?}", listener);
         for stream_result in listener.incoming() {
             let data = data.clone();
@@ -65,4 +114,13 @@ fn main() {
     } else {
         println!("Couldn't create a listener on port 8080");
     }
+}
+
+#[test]
+fn test_length() {
+    assert_eq!(take_length(&mut vec![0b00000000, 0b00000000]), 0);
+    assert_eq!(take_length(&mut vec![0b00000000, 0b00000001]), 1);
+    assert_eq!(take_length(&mut vec![0b00000001, 0b00000000]), 256);
+    assert_eq!(take_length(&mut vec![0b00000001, 0b00000001]), 257);
+    assert_eq!(take_length(&mut vec![0b00000001, 0b00000001, 1]), 257);
 }
---
+++ test.py
@@ -0,0 +1,13 @@
+import paho.mqtt.client as mqtt
+
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.connect("localhost", 1883, 60)
+client.loop_forever()
+
